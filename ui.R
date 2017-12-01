6f5355c9af226a0a6ec7bb2da69290300c380e4f
bczernecki <bczernecki@gmail.com>
2017-12-01 19:33:15 +0100

    init

---
+++ ui.R
@@ -0,0 +1,36 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+
+library(shiny)
+library(plotly)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("CAPE"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins", "Looping Animation:",
+                  min = 1, max = 20,
+                  value = 1, step = 1,
+                  animate =
+                    animationOptions(interval = 800, loop = T))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      uiOutput("czas"),
+      plotOutput("distPlot" , width = "100%")
+    )
+  )
+))
